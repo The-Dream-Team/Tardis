6503450cdd1ebfb9e2fa874de6676bbf7531c5b1
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2013-11-06 10:51:02 +0000

    Build the canvas, add more params, game loop, main!!!

--- src/me/dreamteam/tardis/Game.java
+++ src/me/dreamteam/tardis/Game.java
@@ -1,7 +1,13 @@
 package me.dreamteam.tardis;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
 Main Class
@@ -18,5 +24,71 @@ public class Game extends Canvas {
 	
 	private boolean isRunning = true;
 	// Is the game running or not?
+	
+	private String gameName = "Codename TARDIS ";
+	private String build = "Alpha ";
+	private String version = "0.1";
+	// Version set up so that we can see where we are at
+	
+	
+	public Game() {
+		// create a frame to contain our game
+		JFrame container = new JFrame(gameName + "- " + build + version);
+		
+		// get hold the content of the frame and set up the resolution of the game
+		JPanel panel = (JPanel) container.getContentPane();
+		panel.setPreferredSize(new Dimension(500,650));
+		// Katie feel free to change this to the dimensions as given in the photoshop document
+		panel.setLayout(null);
+		
+		// setup our canvas size and put it into the content of the frame
+		setBounds(0,0,500,650);
+		panel.add(this);
+		
+		// Tell AWT not to bother repainting our canvas since we're
+		// going to do that our self in accelerated mode
+		setIgnoreRepaint(true);
+		
+		// finally make the window visible 
+		container.pack();
+		container.setResizable(false);
+		container.setVisible(true);
+		
+		// add a listener to respond to the user closing the window. If they
+		// do we'd like to exit the game
+		container.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+	
+	/**
+	 * Garbage collection and looping
+	 */
+	private void startGame() {
+	
+	}
+	
+	
+	public void gameLoop() {
+		long lastLoopTime = System.currentTimeMillis();
+		
+		while (isRunning) {
+			long delta = System.currentTimeMillis() - lastLoopTime;
+			lastLoopTime = System.currentTimeMillis();
+			
+		}
+	}
+	
+	/**
+	 * Game Start
+	 */
+	public static void main(String argv[]) {
+		Game g =new Game();
+
+		// Start the main game loop
+		g.gameLoop();
+	}
 	
 }
