3be2ad3ef96bb43fb113bff8a0b006e2cbff136d
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2013-11-06 10:39:14 +0000

    Start adding game parameters

--- src/me/dreamteam/tardis/Game.java
+++ src/me/dreamteam/tardis/Game.java
@@ -1,10 +1,22 @@
 package me.dreamteam.tardis;
 
 import java.awt.Canvas;
+import java.awt.image.BufferStrategy;
 
 /**
 Main Class
  */
 
 public class Game extends Canvas {
+	
+	/**
+	 * Begin the game parameters that will allow us to define certain elements.
+	 */
+	
+	private BufferStrategy strategy;
+	// This provides hardware acceleration
+	
+	private boolean isRunning = true;
+	// Is the game running or not?
+	
 }
