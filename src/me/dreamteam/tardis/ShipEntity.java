8e277dce76f9dde95132cde774ee347805ba75ee
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2013-11-29 19:36:44 +0000

    Cleanup comments

--- src/me/dreamteam/tardis/ShipEntity.java
+++ src/me/dreamteam/tardis/ShipEntity.java
@@ -1,43 +1,19 @@
 package me.dreamteam.tardis;
 
-/**
- * The entity that represents the players ship
- */
-
 public class ShipEntity extends Entity {
-	/** The game in which the ship exists */
 	private Game game;
 	
-	/**
-	 * Create a new entity to represent the players ship
-	 *  
-	 * @param game The game in which the ship is being created
-	 * @param ref The reference to the sprite to show for the ship
-	 * @param x The initial x location of the player's ship
-	 * @param y The initial y location of the player's ship
-	 */
 	public ShipEntity(Game game,String ref,int x,int y) {
 		super(ref,x,y);
 		
 		this.game = game;
 	}
 	
-	/**
-	 * Request that the ship move itself based on an elapsed amount of
-	 * time
-	 * 
-	 * @param delta The time that has elapsed since last move (ms)
-	 */
 	public void move(long delta) {
-		// if we're moving left and have reached the left hand side
-		// of the screen, don't move
-		// !! This will be vital for our game which means we can force the ship to stay within the frame.
 		if ((dx < 0) && (x < 10)) {
 			return;
 		}
-		// if we're moving right and have reached the right hand side
-		// of the screen, don't move
-		// !! This will be vital for our game which means we can force the ship to stay within the frame.
+
 		if ((dx > 0) && (x > 440)) {
 			return;
 		}
@@ -45,15 +21,9 @@ public class ShipEntity extends Entity {
 		super.move(delta);
 	}
 	
-	/**
-	 * Notification that the player's ship has collided with something
-	 * 
-	 * @param other The entity with which the ship has collided
-	 */
 	
 	public void collidedWith(Entity other) {
 	
-		// TBD
-			
+	// To be done at a later date
 		}
 	}
