210ebd86aea317e3596e5f03f8703b7b314af4d2
ping19 <p_sam@live.com>
2013-11-29 12:25:23 +0000

    Coded EnemyEntity class to add new sprites

--- src/me/dreamteam/tardis/EnemyEntity.java
+++ src/me/dreamteam/tardis/EnemyEntity.java
@@ -1,5 +1,17 @@
 package me.dreamteam.tardis;
 
-public class EnemyEntity {
+public class EnemyEntity extends Entity {
+	
+	private Game game;
+	
+	public EnemyEntity(Game game,String ref,int x,int y) {
+		super(ref,x,y);
+		
+		this.game = game;
+	}
 
+	
+	public void collidedWith(Entity other) {
+		
+	}
 }
