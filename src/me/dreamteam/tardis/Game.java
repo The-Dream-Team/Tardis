340cb30f6df1a6470fc51df41698b7284df738e6
katiewood <katie.wood3@hotmail.com>
2013-12-02 11:44:47 +0000

    Add in ship 2 sprite

--- src/me/dreamteam/tardis/Game.java
+++ src/me/dreamteam/tardis/Game.java
@@ -174,7 +174,7 @@ public class Game extends Canvas {
 		entities.add(Enemy);
 		Entity Enemy2 = new EnemyEntity(this,"sprites/ufo.png",250,50);
 		entities.add(Enemy2);
-		Entity Enemy3 = new EnemyEntity(this,"sprites/oneeyem.png",350,50);
+		Entity Enemy3 = new EnemyEntity(this,"sprites/oe1.png",350,50);
 		entities.add(Enemy3);
 	}
 	
