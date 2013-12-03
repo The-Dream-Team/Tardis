4c96a2a72e3ee3a07180784efc0cbaec11f06fc7
katiewood <katie.wood3@hotmail.com>
2013-12-03 13:39:56 +0000

    movement of all enemy entity

--- src/me/dreamteam/tardis/EnemyEntity.java
+++ src/me/dreamteam/tardis/EnemyEntity.java
@@ -1,26 +1,33 @@
 package me.dreamteam.tardis;
 
 public class EnemyEntity extends Entity {
+	
+	// The speed at which the alient moves horizontally 
+    private double moveSpeed = 75;
+	
 	private Game game;
 	
 	public EnemyEntity(Game game,String ref,int x,int y) {
 		super(ref,x,y);
 		
 		this.game = game;
+		dy = moveSpeed;
 	}
 
 	
 	public void move(long delta) {
-		if ((dx < 0) && (x < 10)) {
+		if ((dy < 0) && (y < 10)) {
 			game.updateLogic();
 		}
 
-		if ((dx > 0) && (x > 750)) {
+		if ((dy > 0) && (y > 750)) {
 			game.updateLogic();
 		}
 		
 		super.move(delta);
 	}
+	
+
 	
 	public void collidedWith(Entity other) {
 		
