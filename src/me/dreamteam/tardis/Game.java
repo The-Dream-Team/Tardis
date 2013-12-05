dde6016a656524989cf2cbb22fb2520cc35b26bb
ping19 <p_sam@live.com>
2013-12-05 05:27:00 +0000

    Makes game harder as time passes

--- src/me/dreamteam/tardis/Game.java
+++ src/me/dreamteam/tardis/Game.java
@@ -259,9 +259,23 @@ public class Game extends Canvas {
 		SpriteLoc = rSpriteLoc.nextInt(200);
 		SpriteLoc2 = 200+rSpriteLoc.nextInt(450);
 		if(twait != gameTime){
-			if(gameTime >= twait+2){
+			int FinalLoc;
+			if(gameTime >= twait+2 && gameTime < 90){
 				twait = gameTime;
-				int FinalLoc;
+				for(int i = 0; i<2; i++){
+					if(i==0){
+						FinalLoc = SpriteLoc;
+					}else{
+						FinalLoc = SpriteLoc2;
+					}
+					Entity Enemies = new EnemyEntity(this,"sprites/enemies/0"+CurSprite+".png",FinalLoc,-50);
+					entities.add(Enemies);
+					CurSprite += 1;
+					if (CurSprite>3)
+						CurSprite=1;
+				}
+			}else if (gameTime >= twait && gameTime >= 90){
+				twait = gameTime;
 				for(int i = 0; i<2; i++){
 					if(i==0){
 						FinalLoc = SpriteLoc;
