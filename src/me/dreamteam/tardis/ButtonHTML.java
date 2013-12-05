08a44da62e269e5b9e73c3cb847b3df871b7a8a9
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2013-12-05 22:29:12 +0000

    Cleanup Buttons, Add SL&F to Utils + more

---
+++ src/me/dreamteam/tardis/ButtonHTML.java
@@ -0,0 +1,10 @@
+package me.dreamteam.tardis;
+
+public class ButtonHTML {
+
+	
+	static String bpStart = "<html><img src=\"";
+	static String bpMiddle = "\"></img><br><center>";
+	static String bpEnd = "</center></html>";
+	
+}
