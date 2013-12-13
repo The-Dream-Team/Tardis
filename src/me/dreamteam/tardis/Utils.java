4153100d4683933532e595fe211ead9d331883bc
ping19 <p_sam@live.com>
2013-12-13 03:27:07 +0000

    Revert "Update version"
    This reverts commit ae847f60e007e912602a59b080297c03cd1badfd.

--- src/me/dreamteam/tardis/Utils.java
+++ src/me/dreamteam/tardis/Utils.java
@@ -13,7 +13,7 @@ public class Utils {
 	
 	static String gameName = "Codename TARDIS ";
 	static String build = "Alpha ";
-	static String version = "0.5.1";
+	static String version = "0.5";
 	
 	/*
 	 * Ship Names
