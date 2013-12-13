beee6e3248719cfa47772347ade62268906daefb
ping19 <p_sam@live.com>
2013-12-13 17:24:36 +0000

    Corrected background creation through right class

--- src/me/dreamteam/tardis/Background.java
+++ src/me/dreamteam/tardis/Background.java
@@ -1,11 +1,7 @@
 package me.dreamteam.tardis;
 
 public class Background extends Entity {
-	
-	
 	private Game game;
-	
-	
 	
 	public Background(Game game,String ref,int x,int y) {
 		super(ref,x,y);
