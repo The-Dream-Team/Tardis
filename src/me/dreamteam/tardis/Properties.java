50cfbfb8fb50fd0bd621da348568c539b3ec333d
ping19 <p_sam@live.com>
2013-12-27 19:01:48 +0000

    Auto attack, weapon can only be used once

--- src/me/dreamteam/tardis/Properties.java
+++ src/me/dreamteam/tardis/Properties.java
@@ -73,5 +73,13 @@ public class Properties{
 	public int timeMil;
 	public long lastTime;
 	public int tWait = 0;
+	public long weaponLoopTime;
+	public void setWeapon(int w){
+		weapon1 = w;
+		weaponDisplay = weapon1+"/"+weapon2+"/"+weapon3;
+	}
+	public int getWeapon(){
+		return weapon1;
+	}
 
 }
