d49496c11ac2f70ecf46731dd00870571960688d
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2013-12-29 14:46:20 +0000

    Add more to "How to Play"

--- src/me/dreamteam/tardis/HowToPlay.java
+++ src/me/dreamteam/tardis/HowToPlay.java
@@ -45,11 +45,6 @@ public class HowToPlay extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel htpTitle = new JLabel("Welcome to Super Spaceship Adventure");
-		htpTitle.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		htpTitle.setBounds(10, 72, 306, 23);
-		htpTitle.setForeground(Color.WHITE);
-		contentPane.add(htpTitle);
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -61,28 +56,54 @@ public class HowToPlay extends JFrame {
 		JLabel htpHeading = new JLabel("How to Play...");
 		htpHeading.setForeground(Color.WHITE);
 		htpHeading.setFont(new Font("Tahoma", Font.BOLD, 39));
-		htpHeading.setBounds(10, 11, 430, 59);
+		htpHeading.setBounds(10, 5, 430, 59);
 		contentPane.add(htpHeading);
+		
+		JLabel htpTitle = new JLabel("Welcome to Super Spaceship Adventure");
+		htpTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
+		htpTitle.setBounds(10, 65, 306, 23);
+		htpTitle.setForeground(Color.WHITE);
+		contentPane.add(htpTitle);
 		
 		JLabel htpLine1 = new JLabel("To begin, choose a spaceship.");
 		htpLine1.setForeground(Color.WHITE);
-		htpLine1.setBounds(10, 98, 306, 14);
+		htpLine1.setBounds(10, 100, 306, 14);
 		contentPane.add(htpLine1);
 		
 		JLabel htpLine2 = new JLabel("Your spaceship will guide you through the universe, an universe filled with enemies.");
 		htpLine2.setForeground(Color.WHITE);
-		htpLine2.setBounds(10, 118, 430, 14);
+		htpLine2.setBounds(10, 115, 430, 14);
 		contentPane.add(htpLine2);
 		
 		JLabel htpLine3 = new JLabel("To move your spaceship, use LEFT ARROW KEY or RIGHT ARROW KEY. ");
 		htpLine3.setForeground(Color.WHITE);
-		htpLine3.setBounds(10, 143, 430, 14);
+		htpLine3.setBounds(10, 145, 430, 14);
 		contentPane.add(htpLine3);
 		
 		JLabel htpLine4 = new JLabel("Alternatively, you can use the A KEY to move left, or the D KEY to move right");
 		htpLine4.setForeground(Color.WHITE);
-		htpLine4.setBounds(10, 161, 430, 14);
+		htpLine4.setBounds(10, 160, 430, 14);
 		contentPane.add(htpLine4);
+		
+		JLabel htpLine5 = new JLabel("Sometimes you may get a powerup, use SPACE KEY to use the powerup.");
+		htpLine5.setForeground(Color.WHITE);
+		htpLine5.setBounds(10, 185, 430, 14);
+		contentPane.add(htpLine5);
+		
+		JLabel htpLine6 = new JLabel("To switch between powerups, use NUM 1, NUM 2 or NUM 3");
+		htpLine6.setForeground(Color.WHITE);
+		htpLine6.setBounds(10, 200, 430, 14);
+		contentPane.add(htpLine6);
+		
+		JLabel htpLine7 = new JLabel("Powerups are limited, so use them wisely!");
+		htpLine7.setForeground(Color.WHITE);
+		htpLine7.setBounds(10, 215, 430, 14);
+		contentPane.add(htpLine7);
+		
+		JLabel htpLine8 = new JLabel("To pause the game at any time, use P KEY or ESC KEY");
+		htpLine8.setForeground(Color.WHITE);
+		htpLine8.setBounds(10, 245, 430, 14);
+		contentPane.add(htpLine8);
 		
 		JLabel htpHide = new JLabel("Click here to hide...");
 		htpHide.setForeground(Color.WHITE);
@@ -90,10 +111,11 @@ public class HowToPlay extends JFrame {
 		htpHide.setBounds(220, 364, 220, 25);
 		contentPane.add(htpHide);
 		
-		JLabel lblSometimesYouMay = new JLabel("Sometimes you may get a powerup, use SPACE KEY to use the powerup.");
-		lblSometimesYouMay.setForeground(Color.WHITE);
-		lblSometimesYouMay.setBounds(10, 186, 430, 14);
-		contentPane.add(lblSometimesYouMay);
+		JLabel htpHaveFun = new JLabel("Have fun!");
+		htpHaveFun.setFont(new Font("Tahoma", Font.BOLD, 20));
+		htpHaveFun.setBounds(10, 280, 306, 23);
+		htpHaveFun.setForeground(Color.WHITE);
+		contentPane.add(htpHaveFun);
 		
 		
 	}
