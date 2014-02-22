4fd76cb0fe1be0917f40b30b6dce7e80fde990b1
ping19 <p_sam@live.com>
2014-02-22 22:19:34 +0000

    New class containing the pack methods

---
+++ src/main/java/me/dreamteam/tardis/Pack.java
@@ -0,0 +1,19 @@
+package me.dreamteam.tardis;
+
+public class Pack extends Entity {
+    private Game game;
+
+    public Pack(Game game, String ref, int x, int y) {
+        super(ref, x, y);
+
+        this.game = game;
+    }
+
+    public void move(long delta) {
+
+        super.move(delta);
+    }
+
+    public void collidedWith(Entity other) {
+    }
+}
