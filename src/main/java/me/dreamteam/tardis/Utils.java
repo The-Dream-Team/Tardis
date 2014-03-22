2d50fd4632cdb1edcb125bdb06688fb5ad48052f
ping19 <p_sam@live.com>
2014-03-22 23:38:23 +0000

    Updated msg

--- src/main/java/me/dreamteam/tardis/Utils.java
+++ src/main/java/me/dreamteam/tardis/Utils.java
@@ -138,6 +138,8 @@ public class Utils {
     static String txtWebsite = "Website";
     static String txtWebsiteMsg = "The website will be opened within your web browser. \nDo you wish to continue?";
     static String urlWebsite = "http://the-dreamteam.co.uk";
+    
+    static String txtHighscoreMsg = "Highscore will be displayed on your web browser. \nDo you wish to continue?";
 
     // Quit Game Dialog
     static String txtQuit = "Quit Game";
